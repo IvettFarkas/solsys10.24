52f801e2d851f4b5c08e1c8b9a9e89abb47d616e
Farkas Ivett <farkasivettbianka16@gmail.com>
2023-10-24 13:53:25 +0200

    Add project files.

---
+++ Bolygo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solsys10._24
+{
+    class Bolygo
+    {
+        public string BolygoNeve { get; set; }
+        public int HoldjainakSzama { get; set; }
+        public double TerfogatAranya { get; set; }
+
+        public Bolygo(string sor)
+        {
+            List<string> atmeneti = sor.Split(';').ToList();
+
+            BolygoNeve = atmeneti[0];
+            HoldjainakSzama = Convert.ToInt32(atmeneti[1]);
+            TerfogatAranya = double.Parse(atmeneti[2].Replace('.', ','));
+        }
+    }
+}
